9f677a2d59cdd58647544909ccc6d463d676f248
SunsetFoxHound <brenoluca046@gmail.com>
2023-06-16 20:16:22 -0300

    Resolvi fazerÊ

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Insira um número para ver sua tabuada: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int b = 0;
+int produto;
+
+while (b <= 10)
+{
+    produto = a * b;
+    Console.WriteLine($"{a} x {b} = {produto}");
+    b += 1;
+}
